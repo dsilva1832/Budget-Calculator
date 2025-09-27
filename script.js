c10bd95f2cebeb15e34da1d1da0eeea43cdde8ed
dsilva1832 <alwindisilva2014@gmail.com>
2025-09-27 15:32:45 +0530

    Budget v3

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function register(){
     user={
         username:uname.value,
         email:email.value,
-        password:pswd.value
+        password:pswd.value,
+        income:0,
+        expense:0
     }
     console.log(user.username,user.email,user.password);
     if(user.username==''||user.email==''||user.password==''){
@@ -25,20 +27,30 @@ function login(){
         alert("Enter all fields");
     }else{
         if(usrname in localStorage){
-            localStorage.setItem("USER",usrname)
-            window.location='./home.html'
-        }else{
-            alert("User does not exist! Please Register")
-        }
-    }
+        //     localStorage.setItem("USER",usrname)
+        //     localStorage.setItem("PASS",paswrd);
+        //     window.location='./home.html'
+        // }else{
+        //     alert("Wrong credentials!!")
+        //     alert("Please register if you're new!!")
+        // }
 
-    // if(pswd==user.password){
-    //     localStorage.setItem("Password",pswd);
-    //     alert("Login Success");
-    // }else{
-    //     alert("Wrong password");
-    // }
-    
+            let newObj=JSON.parse(localStorage.getItem(usrname));
+            console.log(newObj);
+            if(paswrd===newObj.password){
+                localStorage.setItem('loggedobj',JSON.stringify(newObj))
+                localStorage.setItem("loggedkey",usrname)
+
+                alert("Login Successful")
+                window.location='./home.html';
+            }else{
+                alert("Wrong Password: login failed")
+                document.getElementById("password").value=" "
+            }
+        }else{
+            alert("User does not exist. Please Register")
+        }
+    }  
 }
 
 // welcome user
@@ -48,4 +60,24 @@ welcome.innerHTML=`Welcome ${loginUser}`
 function logout(){
     localStorage.clear()
     window.location='index.html'
+}
+
+function addIncome(event){
+    // event.preventDefault();
+    let incometype=document.getElementById('incometype').value
+    let incomeamt=document.getElementById('incomeamt').value
+   
+    if(incomeamt==''||incometype==''){
+        alert("Enter all fields");
+    }else{
+        let newobj=JSON.parse(localStorage.getItem(user.username))
+        console.log(newobj);
+        
+        incomedisplay.innerHTML=`${incomeamt} Rs`
+    }
+    // incomedisplay.innerHTML=``
+}
+
+function addExpense(){
+
 }
