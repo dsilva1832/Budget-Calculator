2b238544f1f4d12682ccf89465129b770ac0245d
dsilva1832 <alwindisilva2014@gmail.com>
2025-09-27 14:20:32 +0530

    Budget v2

--- script.js
+++ script.js
@@ -4,19 +4,40 @@ function register(){
         email:email.value,
         password:pswd.value
     }
-    if(user.username in localStorage){
-        alert("User already exist")
+    console.log(user.username,user.email,user.password);
+    if(user.username==''||user.email==''||user.password==''){
+        alert("Enter all fields")
     }else{
-        localStorage.setItem(user.username,JSON.stringify(user))
-        alert("User successfully registered")
-        window.location="./index.html"
+        if(user.username in localStorage){
+            alert("Username already exist")
+        }else{
+            localStorage.setItem(user.username,JSON.stringify(user))
+            alert("User successfully registered")
+            window.location="./index.html"
+        }
     }
 }
 
 function login(){
     usrname=uname.value;
-    localStorage.setItem("USER",usrname)
-    window.location='./home.html'
+    paswrd=lpswd.value;
+    if(usrname==''||paswrd==''){
+        alert("Enter all fields");
+    }else{
+        if(usrname in localStorage){
+            localStorage.setItem("USER",usrname)
+            window.location='./home.html'
+        }else{
+            alert("User does not exist! Please Register")
+        }
+    }
+
+    // if(pswd==user.password){
+    //     localStorage.setItem("Password",pswd);
+    //     alert("Login Success");
+    // }else{
+    //     alert("Wrong password");
+    // }
     
 }
 
