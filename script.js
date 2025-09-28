cd4850a9bbb3cdbe16c6efb01245d8a44a0e64a3
dsilva1832 <alwindisilva2014@gmail.com>
2025-09-28 13:35:07 +0530

    Budget V4

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 function register(){
     user={
-        username:uname.value,
+        uname:uname.value,
         email:email.value,
         password:pswd.value,
         income:0,
         expense:0
     }
-    console.log(user.username,user.email,user.password);
-    if(user.username==''||user.email==''||user.password==''){
+    console.log(user.uname,user.email,user.password);
+    if(user.uame==''||user.email==''||user.password==''){
         alert("Enter all fields")
     }else{
-        if(user.username in localStorage){
+        if(user.uname in localStorage){
             alert("Username already exist")
         }else{
-            localStorage.setItem(user.username,JSON.stringify(user))
+            localStorage.setItem(user.uname,JSON.stringify(user))
             alert("User successfully registered")
             window.location="./index.html"
         }
     }
 }
+
+displayIncomeExpense();
+
 
 function login(){
     usrname=uname.value;
@@ -27,13 +30,7 @@ function login(){
         alert("Enter all fields");
     }else{
         if(usrname in localStorage){
-        //     localStorage.setItem("USER",usrname)
-        //     localStorage.setItem("PASS",paswrd);
-        //     window.location='./home.html'
-        // }else{
-        //     alert("Wrong credentials!!")
-        //     alert("Please register if you're new!!")
-        // }
+     
 
             let newObj=JSON.parse(localStorage.getItem(usrname));
             console.log(newObj);
@@ -54,30 +51,81 @@ function login(){
 }
 
 // welcome user
-let loginUser=localStorage.getItem('USER')
-welcome.innerHTML=`Welcome ${loginUser}`
+let loginUser=localStorage.getItem('loggedkey')
+welcome.innerHTML=`Welcome ${loginUser.toUpperCase()}`
 
 function logout(){
     localStorage.clear()
     window.location='index.html'
 }
 
+function displayIncomeExpense(){
+    let loggedkey = localStorage.getItem("loggedkey");
+    let obj=JSON.parse(localStorage.getItem(loggedkey));
+    let originalobj=JSON.parse(localStorage.getItem(obj.uname))
+    let incomedisplay=document.getElementById("incomedisplay")
+    let expensedisplay=document.getElementById("expensedisplay")
+    incomedisplay.innerHTML=`Rs ${originalobj.income}/-`
+    expensedisplay.innerHTML=`Rs ${originalobj.expense}/-`
+}
+
 function addIncome(event){
-    // event.preventDefault();
+    event.preventDefault();
     let incometype=document.getElementById('incometype').value
     let incomeamt=document.getElementById('incomeamt').value
    
     if(incomeamt==''||incometype==''){
         alert("Enter all fields");
     }else{
-        let newobj=JSON.parse(localStorage.getItem(user.username))
+        let loggedkey = localStorage.getItem("loggedkey");
+        let newobj=JSON.parse(localStorage.getItem(loggedkey))
+        newobj.income=newobj.income+parseFloat(incomeamt)
         console.log(newobj);
         
-        incomedisplay.innerHTML=`${incomeamt} Rs`
+        localStorage.setItem(newobj.uname,JSON.stringify(newobj))
+        alert("Income added successfully")
+        displayIncomeExpense();
+        document.getElementById('incomeform').reset()
+
     }
-    // incomedisplay.innerHTML=``
 }
 
-function addExpense(){
+
+function addExpense(event){
+    event.preventDefault();
+    let expensetype=document.getElementById("expensetype").value
+    let expenseamt=document.getElementById("expenseamt").value
+    if(expensetype==''|| expenseamt==''){
+        alert("Enter All Fields")
+    }
+    else{
+        let loggedkey = localStorage.getItem("loggedkey");
+        let newobj=JSON.parse(localStorage.getItem(loggedkey))
+        if(expenseamt >newobj.income){
+            alert("Insufficient amount")
+        }
+        else{
+            newobj.income=newobj.income-parseFloat(expenseamt)
+            newobj.expense=newobj.expense+parseFloat(expenseamt)
+            localStorage.setItem(newobj.uname,JSON.stringify(newobj))
+            alert("expense added successfully")
+            document.getElementById("expenseform").reset();
+            displayIncomeExpense();
+            // location.reload();
+        }
+    }
+}
+
+
+function clearAll(){
+    let clear = confirm("Are you sure to clear all data?")
+    if(clear){
+        let loggedkey=localStorage.getItem('loggedkey')
+        let newobj=JSON.parse(localStorage.getItem(loggedkey))
+        newobj.income=0;
+        newobj.expense=0;
+        localStorage.setItem(newobj.uname,JSON.stringify(newobj))
+        displayIncomeExpense();
+    }
 
 }
